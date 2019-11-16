4ec101454a8e17b49c678ac330e059da063b4f5e
Fabbbe <fabbbe166@gmail.com>
2019-11-16 15:07:43 +0100

    Added ability to load from file.

--- brainfuck_interpreter.c
+++ brainfuck_interpreter.c
@@ -2,14 +2,48 @@
  *
  * A small brainfuck interpreter written in C
  *
+ * It works with nested loops but has no error checking
+ *
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-const char* code = "++++++++[>++++++++<-]>+."; // Should output "A"
+const char* load_brainfuck(char* filepath) {
+	char* buffer = 0;
+	long length;
+	FILE* file_ptr = fopen (filepath, "rb");
 
-int main (void) {
-	char program_data[256]; // this limits brainfuck to 256 cells
+	if (!file_ptr) {
+		printf("File does not exist!\n");
+		exit(1);
+	} else {
+		fseek(file_ptr, 0, SEEK_END);
+		length = ftell(file_ptr);
+		fseek(file_ptr, 0, SEEK_SET);
+		buffer = malloc(length);
+		if (buffer) {
+			fread(buffer, 1, length, file_ptr);
+		}
+		fclose(file_ptr);
+	}
+	return (const char*)buffer;
+}
+
+int main (unsigned int argc, char** argv) {
+	
+	// get filepath
+	char* filepath;
+	if (argc > 1) {
+		filepath = argv[1];
+	} else {
+		printf("No file specified\n");
+		exit(1);
+	}
+
+	const char* code = load_brainfuck(filepath);
+
+	char program_data[256]; // this limits brainfuck to 256 cells :-/
 	char* data_ptr = (char*)program_data;
 	char* code_ptr = (char*)code;
 
@@ -26,15 +60,14 @@ int main (void) {
 			case '-':
 				--*data_ptr;
 				break;
-			case '.':
-				printf("%c", *data_ptr);
-				break;
+			
 			case '>':
 				++data_ptr;
 				break;
 			case '<':
 				--data_ptr;
 				break;
+
 			case '[':
 				loop_ptr_array[loop_ptr_ptr - loop_ptr_array] = (char*)(code_ptr);
 				++loop_ptr_ptr;
@@ -46,10 +79,15 @@ int main (void) {
 					--loop_ptr_ptr;
 				}
 				break;
+
+			case '.':
+				printf("%c", *data_ptr);
+				break; 
+			// TODO: add input
+			default:
+				break;
 		}
 		++code_ptr;
 	}
-
-	printf("\n");
 	return 0;
 }
---
+++ helloworld.bf
@@ -0,0 +1,7 @@
+This is hello world written in brainfuck
+
+All charachters are ignored except for the
+ones used by brainfuck
+
+Here's the code:
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.
